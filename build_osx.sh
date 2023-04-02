17781c2dd7d74485c7f2d7e3af7fa74c00baa3bf
Frank Harrison <frank@doublethefish.com>
2023-04-02 13:42:15 +0100

    chore(standalone-odgf): set DCMAKE_OSX_ARCHITECTURES to crosscompile all platforms

--- build_osx.sh
+++ build_osx.sh
@@ -8,7 +8,7 @@ brew install libomp || brew reinstall libomp
 cd ./ogdf-conda/src
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./libOGDFTmap -DBUILD_SHARED_LIBS=ON
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DCMAKE_INSTALL_PREFIX=./libOGDFTmap -DBUILD_SHARED_LIBS=ON
 make -j4
 make install
 cd ..
